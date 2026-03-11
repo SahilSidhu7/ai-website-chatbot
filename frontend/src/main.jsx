5e0d4f9ecdaf2520463ed5c4d831100bb57bc025
SahilSidhu7 <sahilsidhu3127@gmail.com>
2026-03-11 21:06:53 +0530

    fixed the widget issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const container = document.getElementById("ai-chatbot-widget");
+
+if (container) {
+  ReactDOM.createRoot(container).render(<App />);
+}
